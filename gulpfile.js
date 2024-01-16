3382fc710fbbd82c729437ccc322ad891a484001
vikkuccine <kamenkova09@gmail.com>
2024-01-17 00:31:14 +0200

    header first-block

---
+++ gulpfile.js
@@ -0,0 +1,115 @@
+const { src, dest, watch, parallel, series } = require('gulp')
+
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+const clean = require('gulp-clean');
+const avif = require('gulp-avif');
+const webp = require('gulp-webp');
+const imagemin = require('gulp-imagemin');
+const newer = require('gulp-newer');
+const fonter = require('gulp-fonter');
+const ttf2woff2 = require('gulp-ttf2woff2');
+const include = require('gulp-include');
+const postcss = require('gulp-postcss');
+const tailwindcss = require('tailwindcss');
+
+function pages() {
+    return src('src/pages/*.html')
+        .pipe(include({
+            includePaths: 'src/components'
+        }))
+        .pipe(dest('app'))
+        .pipe(browserSync.stream())
+}
+
+function fonts() {
+    return src('src/fonts/*.*')
+        .pipe(fonter({
+            formats: ['woff', 'ttf']
+        }))
+        .pipe(src('src/fonts/*.ttf'))
+        .pipe(ttf2woff2())
+        .pipe(dest('app/fonts'))
+}
+
+function images() {
+    return src(['src/images/*.*'])
+        .pipe(newer('app/images'))
+        .pipe(avif({ guality: 50 }))
+
+        .pipe(src('src/images/*.*'))
+        .pipe(newer('app/images'))
+        .pipe(webp())
+
+        .pipe(src('src/images/*.*'))
+        .pipe(newer('app/images'))
+        .pipe(imagemin())
+        .pipe(dest('app/images'))
+}
+
+function scripts() {
+    return src([
+        'src/js/main.js'
+    ])
+        .pipe(concat('main.min.js'))
+        // .pipe(uglify())
+        .pipe(dest('app/js'))
+        .pipe(browserSync.stream())
+}
+
+function styles() {
+    const processors = [
+        tailwindcss('./tailwind.config.js'),
+    ];
+    return src('src/scss/style.scss')
+        .pipe(autoprefixer({ overrideBrowserslist: ['last 10 version'] }))
+        .pipe(concat('style.min.css'))
+        .pipe(postcss(processors))
+        .pipe(scss({ outputStyle: 'compressed' }))
+        .pipe(dest('app/css'))
+        .pipe(browserSync.stream())
+}
+
+function watching() {
+    browserSync.init({
+        server: {
+            baseDir: "app/"
+        }
+    });
+    watch(['src/scss/**/*.scss'], styles)
+    watch(['src/images'], images)
+    watch(['src/js/main.js'], scripts)
+    watch(['src/components/*', 'src/pages/*'], pages)
+    watch(['app/*.html'], styles).on('change', browserSync.reload)
+}
+
+
+function cleanDist() {
+    return src('dist')
+        .pipe(clean())
+}
+
+function building() {
+    return src([
+        'app/css/style.min.css',
+        'app/images/*.*',
+        'app/fonts/*.*',
+        'app/js/main.min.js',
+        'app/**/*.html'
+    ], { base: 'app' })
+        .pipe(dest('dist'))
+}
+
+exports.styles = styles;
+exports.images = images;
+exports.scripts = scripts;
+exports.building = building;
+exports.watching = watching;
+exports.fonts = fonts;
+exports.pages = pages;
+
+exports.build = series(cleanDist, building)
+exports.default = parallel(styles, images, scripts, pages, watching)
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,scss}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#588A3D',
+      }
+    },
+  },
+  plugins: [],
+}
+
